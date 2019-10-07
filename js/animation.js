55a643cafa56c1a1004790cb0434b776306129cc
Oleksandr <kyryk.aleks@gmail.com>
2019-10-07 22:30:38 +0300

    emploiplusjs

--- js/animation.js
+++ js/animation.js
@@ -30,6 +30,52 @@
             })
         }
 
+        function openBlock(){
+            let generalBlock = document.querySelector(".general_block");
+let block_hove = document.querySelector(".block_hover");
+let block_opacity = document.querySelector(".block_opacity");
+ block_hove.addEventListener("mouseover", function(){
+ 
+ animate(100, 200,1, 0, 1, 0.01);
+ 
+ 
+  
+ })
+  block_hove.addEventListener("mouseout", function(){
+ 	
+  animate(200, 100,-1, 1, 0.01, -0.01);
+ })
+ 
+ function animate(start, end, cross,opasity, noneOpacity, opacityCross){
+ 	  let id = setInterval(frame, 10);
+  let pos =start;
+  
+  function frame() {
+    if (pos == end) {
+    blockOpacity(opasity, noneOpacity, opacityCross);
+      clearInterval(id);
+    } else {
+      pos=pos + cross;
+      generalBlock.style.height = pos + 'px';
+    }
+    }
+ }
+ 
+ function blockOpacity(opasity, noneOpacity, opacityCross){
+ 	  let id = setInterval(frame, 10);
+  let pos =opasity;
+  
+  function frame() {
+    if (pos == noneOpacity) {
+      clearInterval(id);
+    } else {
+      pos=pos + opacityCross;
+      block_opacity.style.opacity = pos;
+    }
+    }
+ }
+        }
+
        
         })
 }());
